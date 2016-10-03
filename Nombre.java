a10ed00bdb76f00c6d3f9ca22cb22c2c7e6a660c
AzaharaMk <azahara.mk@gmail.com>
2016-10-03 12:45:38 +0200

    Nombre en pantalla

---
+++ Nombre.java
@@ -0,0 +1,9 @@
+//Escribe y ejecuta un programa Java que muestre tu nombre completo por pantalla.
+
+public class Nombre
+{
+	public static void main (String args[])
+	{
+		System.out.println("Azahara Garcia");
+	}
+}
